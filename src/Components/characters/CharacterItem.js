8948deba45d6de76519c2de98c3ecc0b579271fe
nitin-codr <nitinporwal99@gmail.com>
2020-07-03 00:54:17 +0530

    add card with styling

--- src/Components/characters/CharacterItem.js
+++ src/Components/characters/CharacterItem.js
@@ -4,11 +4,32 @@ class CharacterItem extends Component {
     render() {
         console.log(this.props.item);
         return (
-            <div>
-                {this.props.item.name}
+            <div className='card'>
+                <div className='card-inner'>
+                    <div className='card-front'>
+                        <img src={this.props.item.img} alt={this.props.item.portrayed} />
+                    </div>
+                    <div className='card-back'>
+                        <h1>{this.props.item.name}</h1>
+                        <ul>
+                            <li>
+                                <strong>Actor Name:</strong> {this.props.item.portrayed}
+                            </li>
+                            <li>
+                                <strong>Nickname:</strong> {this.props.item.nickname}
+                            </li>
+                            <li>
+                                <strong>Birthday:</strong> {this.props.item.birthday}
+                            </li>
+                            <li>
+                                <strong>Status:</strong> {this.props.item.status}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default CharacterItem
+export default CharacterItem;
