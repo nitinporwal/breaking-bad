eba22ecffb06b036451fe66725262110b736b719
nitin-codr <nitinporwal99@gmail.com>
2020-07-03 00:17:11 +0530

    add axios request to fetch data from breaking bad api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import Header from './Components/ui/Header';
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  state = {
+    items: {},
+    isLoading: true
+  }
+
+  componentDidMount = () => {
+    axios.get(`https://www.breakingbadapi.com/api/characters`).then(result => {
+      console.log(result);
+      this.setState({items: result.data, isLoading: false})
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
